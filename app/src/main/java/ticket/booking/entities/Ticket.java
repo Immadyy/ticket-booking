856b717233ffffd3b9fe6563654c4bb2f7bff2d0
Immady <ahmadali63288@gmail.com>
2026-06-02 19:00:00 +0530

    add constructor, getter and setter methods in all entities also add login method in UserBookingService.java

--- app/src/main/java/ticket/booking/entities/Ticket.java
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -7,6 +7,68 @@ public class Ticket {
     private String userID;
     private String source;
     private String destination;
-    private ZonedDateTime dateOfTravel;
+    private String dateOfTravel;
     private Train train;
+
+    public Ticket(String ticketId, String userId, String source, String destination, String dateOfTravel, Train train){
+        this.ticketID = ticketId;
+        this.userID = userId;
+        this.source = source;
+        this.destination = destination;
+        this.dateOfTravel = dateOfTravel;
+        this.train = train;
+    }
+
+    public String getTicketInfo(){
+        return String.format("Ticket ID: %s belongs to User %s from %s to %s on %s", ticketID, userID, source, destination, dateOfTravel);
+    }
+
+    public String getTicketId(){
+        return ticketID;
+    }
+
+    public void setTicketId(String ticketId){
+        this.ticketID = ticketId;
+    }
+
+    public String getSource(){
+        return source;
+    }
+
+    public void setSource(String source){
+        this.source = source;
+    }
+
+    public String getUserId(){
+        return userID;
+    }
+
+    public void setUserId(String userId){
+        this.userID = userId;
+    }
+
+    public String getDestination(){
+        return destination;
+    }
+
+    public void setDestination(String destination){
+        this.destination = destination;
+    }
+
+    public String getDateOfTravel(){
+        return dateOfTravel;
+    }
+
+    public void setDateOfTravel(String dateOfTravel){
+        this.dateOfTravel = dateOfTravel;
+    }
+
+    public Train getTrain(){
+        return train;
+    }
+
+    public void setTrain(Train train){
+        this.train = train;
+    }
+
 }
--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -7,17 +7,29 @@ import ticket.booking.entities.User;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
 
     private User user;
     private List<User> userList;
     private ObjectMapper objectMapper = new ObjectMapper();
-    private static final String USERS_PATH = "../localDb/users.json";
+    private static final String USERS_FILE_PATH = "app/src/main/java/ticket/booking/localDb/users.json";
 
     public UserBookingService(User user1) throws IOException {
         this.user = user1;
-        File users = new File(USERS_PATH);
-        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+        loadUserListFromFile();
     }
+
+    public void loadUserListFromFile() throws IOException {
+        userList = objectMapper.readValue(USERS_FILE_PATH, new TypeReference<List<User>>() {});
+    }
+
+    public  boolean loginUser() {
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
 }
